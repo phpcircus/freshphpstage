04d1598d64853c9fbeee80740fe26d7a71fdb015
Clayton Stone <devcircus@outlook.com>
2019-05-19 18:07:05 -0500

    Tweak site logo and default layout.

--- resources/js/Shared/Logo.vue
+++ resources/js/Shared/Logo.vue
@@ -1,3 +1,5 @@
 <template>
-    <h1 class="inline-block font-bold text-2xl sm:text-3xl md:text-5xl text-blue-500 site-title">PHP<span class="font-medium text-gray-900">STAGE</span></h1>
+    <div class="w-full mx-auto text-center">
+        <h1 class="inline-block font-bold text-2xl sm:text-3xl md:text-5xl text-blue-500 site-title">PHP<span class="font-medium text-white">STAGE</span></h1>
+    </div>
 </template>
