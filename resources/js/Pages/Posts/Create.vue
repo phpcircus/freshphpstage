a86c6368a5ccdbca4e6606948e22fde194a70c56
Clayton Stone <devcircus@outlook.com>
2019-05-24 09:12:30 -0500

    Fix more styles on post show page.

--- resources/js/Pages/Posts/Create.vue
+++ resources/js/Pages/Posts/Create.vue
@@ -5,11 +5,13 @@
             <span class="text-blue-300 font-medium">/</span> Create
         </h1>
         <div class="bg-white rounded shadow overflow-hidden w-full">
-            <form class="w-3/4" @submit.prevent="submit">
-                <div class="p-8 -mr-6 -mb-8 flex flex-col">
-                    <text-input v-model="form.title" :errors="errors.title" class="pr-6 pb-8 w-full lg:w-1/2" label="Post Title" />
-                    <textarea-input v-model="form.summary" :errors="errors.summary" class="pr-6 pb-8 w-full lg:w-1/2" rows="2" label="Post Summary" />
-                    <vue-trix v-model="form.body" placeholder="Enter content" />
+            <form class="w-full md:w-3/4" @submit.prevent="submit">
+                <div class="p-8 -mb-8 flex flex-col">
+                    <text-input v-model="form.title" :errors="errors.title" class="pb-8 w-full lg:w-1/2" label="Post Title" />
+                    <textarea-input v-model="form.summary" :errors="errors.summary" class="pb-8 w-full lg:w-1/2" rows="2" label="Post Summary" />
+                    <div class="w-full md:w-3/4">
+                        <vue-trix v-model="form.body" placeholder="Enter content" />
+                    </div>
                 </div>
                 <div class="px-8 py-4 bg-gray-100 border-t border-gray-200 flex justify-end items-center">
                     <loading-button :loading="sending" class="btn-blue" type="submit">Create Post</loading-button>
