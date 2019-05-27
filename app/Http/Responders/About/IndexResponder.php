366b070126d14dc6208cfd7bb8444f221b3ea082
Clayton Stone <devcircus@outlook.com>
2019-05-27 16:35:13 -0500

    Update about page and about page meta.

--- app/Http/Responders/About/IndexResponder.php
+++ app/Http/Responders/About/IndexResponder.php
@@ -14,6 +14,11 @@ class IndexResponder extends Responder
      */
     public function respond()
     {
-        return Inertia::render('About/Index');
+        return Inertia::render('About/Index')->withViewData('meta', [
+            'twitter_card_url' => config('app.url').'/about/',
+            'twitter_card_title' => 'A Little About Me',
+            'twitter_card_description' => 'A little information about me, and my current web development stack.',
+        ]
+    );
     }
 }
